cdf68967fdff0c8d0cfd196f60476bb76ec89615
Loic Rouaud <loic.rouaud@gmail.com>
2024-06-28 09:40:28 +0200

    URL scraper into OOP file

--- utils_scraper/url_scraper.py
+++ utils_scraper/url_scraper.py
@@ -1,33 +1,37 @@
+# script to export URLs from the immoweb webstire
 import cloudscraper
 from bs4 import BeautifulSoup
 import json
 
-# Initialize the scraper
-scraper = cloudscraper.create_scraper()
+class ImmowebScraper:
+    def __init__(self, status_code=200):
+        self.scraper = cloudscraper.create_scraper()
+        self.root_url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page="
+        self.status_code = status_code
+        self.url_list = []
 
-# Root URL of Immoweb website to use and URL suffix
-root_url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page="
+    def scrape_urls(self):
+        page = 330
+        while self.status_code == 200:
+            url = f"{self.root_url}{page}"
+            html = self.scraper.get(url)
+            self.status_code = html.status_code
 
-page = 333
-status = 200
-url_dict = []
-url_count = 1
+            soup = BeautifulSoup(html.text, 'html.parser')
+            for h2 in soup.find_all('h2', class_='card__title card--result__title'):
+                for link in h2.find_all('a', class_='card__title-link'):
+                    url = link.get('href')
+                    self.url_list.append(url)
+            page += 1
+            print(page, self.status_code)
 
-while status == 200:
-    html = scraper.get(f"{root_url}{page}")
-    status = html.status_code
-    print(page, status)
 
-    soup = BeautifulSoup(html.text, 'html.parser')
+    def save_urls_to_json(self, filename='links.json'):
+        with open(filename, 'w') as json_file:
+            json.dump(self.url_list, json_file, indent=4)
 
-    for h2 in soup.find_all('h2', class_='card__title card--result__title'):
-        for link in h2.find_all('a', class_='card__title-link'):
-            url = link.get('href')
-            url_dict.append(url)
-            url_count += 1
-    page += 1
+obj = ImmowebScraper(200)
 
-# Write the collected URLs to a JSON file
-with open('links.json', 'w') as json_file:
-    json.dump(url_dict, json_file, indent=4)
+obj.scrape_urls()
+obj.save_urls_to_json()
 
